3b8c543f5cd5de567ddedbc7511e6573c0635a68
kramsey458 <kramsey458@gmail.com>
2017-10-22 14:15:26 -0700

    Rating is now correctly changed after trip is complete. Added TripLogger.java to write trip detials into json file.

--- Driver.java
+++ Driver.java
@@ -1,20 +1,72 @@
 //test comment mac
-public class Driver extends Passenger{
+public class Driver{
 
 
-    private String CarTitle;
+    private String carTitle;
     private boolean occupied;
     private Rating rating;
+    private double rank;
+    private String name;
+    private double balance;
+    private Coordinates coordinates;
+    private Map map;
 
-    public Driver(String name, double balance, int x, int y, String CarTitle, boolean occupied){
-        super(name, balance, x, y);
-        this.CarTitle = CarTitle;
+    public Driver(String name, double balance, int x, int y, String CarTitle, boolean occupied, Map map){
+        this.name = name;
+        this.balance = balance;
+        if ((x > 299 || x < 0) || (y > 299 || y < 0))
+            this.coordinates = new Coordinates(0, 0);
+        else
+            this.coordinates = new Coordinates(x, y);
+        this.map = map;
+        this.carTitle = CarTitle;
         this.occupied = occupied;
-        this.rating = new Rating(0,0 );
+        this.rating = new Rating(5,1 );
+        map.addDriver(this);
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public void setRating(Rating rating) {
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public Map getMap() {
+        return map;
+    }
+
+    public void setMap(Map map) {
+        this.map = map;
     }
 
     public String getCarTitle() {
-        return CarTitle;
+        return carTitle;
     }
 
     public boolean isOccupied() {
@@ -22,10 +74,19 @@ public class Driver extends Passenger{
     }
 
     public void setCarTitle(String carTitle) {
-        CarTitle = carTitle;
+        this.carTitle = carTitle;
     }
 
     public void setOccupied(boolean occupied) {
         this.occupied = occupied;
     }
+
+    public double getRank() {
+        return rank;
+    }
+
+    public void setRank(double rank) {
+        this.rank = rank;
+    }
+
 }
