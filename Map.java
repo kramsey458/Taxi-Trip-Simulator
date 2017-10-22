a7faec4db09c7fd8af6f698a3039eebb9261313a
kramsey458 <kramsey458@gmail.com>
2017-10-21 22:47:44 -0700

    Updated Map and Trip class methods. Timer sucessfully implemented.

--- Map.java
+++ Map.java
@@ -1,23 +1,23 @@
 import java.util.*;
 
+
 public class Map {
 
-    public Driver[][] miniMap;
-    private ArrayList<Driver> driverList;
+    public Passenger[][] miniMap;
+    public ArrayList<Driver> driverList;
+    public ArrayList<Passenger> passengerList;
     public Map() {
-        this.miniMap = new Driver[300][300];
+        this.miniMap = new Passenger[300][300];
         driverList = new ArrayList<Driver>();
+        passengerList = new ArrayList<Passenger>();
     }
 
-    public void clearArray( Driver[][] map) {
-        for (Driver[] row: map)
+    public void clearArray( Passenger[][] map) {
+        for (Passenger[] row: map)
             Arrays.fill(row, null);
-        System.out.println(Arrays.deepToString(miniMap));
+        //System.out.println(Arrays.deepToString(miniMap));
     }
 
-    public Driver calculateDriverMatch(Passenger client){
-        return null;
-    }
 
     public double calculateCost(Driver driver, Passenger passenger){
         return 1;
@@ -26,10 +26,51 @@ public class Map {
     public void addDriver(Driver driver){
         miniMap[driver.coordinates.getxCoord()][driver.coordinates.getyCoord()] = driver;
         driverList.add(driver);
-        System.out.println(driverList.get(0).getName());
+        System.out.println(driver.getName());
     }
 
+    public void addPassenger(Passenger passenger){
+        miniMap[passenger.coordinates.getxCoord()][passenger.coordinates.getyCoord()] = passenger;
+        passengerList.add(passenger);
+        System.out.println(passenger.getName());
+    }
 
+    public PriorityQueue<Driver> shortestDriver(Passenger passenger){
+        double distance;
+        PriorityQueue<Driver> driverQueue=new PriorityQueue<Driver>(driverList.size(), new DriverComparator());
+        for (Driver driver : driverList) {
+            if (driver.isOccupied() == false) {
+                distance = driver.coordinates.calculateDistance(passenger.coordinates);
+                //if (Math.abs(distance) < Math.abs(shortestDistance)){
+                driver.setRank(Math.abs(distance));
+                //shortestDistance = distance;
+                driverQueue.add(driver);
+                //}
+            }
+        }
+        System.out.println("Closest " + driverQueue.peek().getName());
+        return driverQueue;
+    }
+
+    public Trip requestRide(Passenger passenger, Coordinates destination) {
+        PriorityQueue<Driver> driverQueue = shortestDriver(passenger);
+        Driver assignedDriver = acceptRide(driverQueue);
+        Trip passengerTrip = new Trip(destination, assignedDriver, passenger, "Nothing");
+        double cost = passengerTrip.calculateCost();
+        passenger.pay(cost, assignedDriver);
+        RideTimer rideTimer = new RideTimer((int)(cost);
+        return passengerTrip;
+    }
+
+    public Driver acceptRide(PriorityQueue<Driver> drivers){
+        PriorityQueue<Driver> driverQueue = drivers;
+        for (Driver driver : driverQueue) {
+            if (driver.isOccupied() == false){
+                return driver;
+            }
+        }
+        return null;
+    }
 
 
 }
