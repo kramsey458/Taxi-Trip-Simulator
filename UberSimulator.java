e1d86f2e6b2e28ba9b98decca066b73eba6a58b5
kramsey458 <kramsey458@gmail.com>
2017-10-21 23:31:11 -0700

    Updated class methods

--- UberSimulator.java
+++ UberSimulator.java
@@ -3,26 +3,32 @@ public class UberSimulator {
 
     public static void main(String[] args){
         Map newMap = new Map();
-        newMap.clearArray(newMap.miniMap);
-        Driver testDriver4 = new Driver("blake", 10, 299, 299,"camry", false);
-        Driver testDriver = new Driver("boris", 10, 0, 0,"camry", false);
-        Driver testDriver2 = new Driver("kyler", 10, 20, 20 ,"camry", false);
-        Driver testDriver3 = new Driver("mac", 10, 100, 100 ,"camry", false);
-        Passenger testPassenger = new Passenger("billy", 100, 10, 10);
+        //newMap.clearArray(newMap.miniMap);
+        Driver testDriver4 = new Driver("blake", 10, 299, 299,"camry", false, newMap);
+        Driver testDriver = new Driver("boris", 10, 14, 14,"camry", false, newMap);
+        Driver testDriver2 = new Driver("kyler", 10, 15, 15 ,"camry", false, newMap);
+        Driver testDriver3 = new Driver("mac", 10, 100, 100 ,"camry", false, newMap);
+        Passenger testPassenger = new Passenger("billy", 150, 15, 15, newMap);
+        Passenger testPassenger2 = new Passenger("Lord choris", 150, 150, 150, newMap);
+        /*
         testDriver2.rating.setTotalRates(5);
         testDriver2.rating.setTotalStars(25);
         testDriver.rating.setTotalStars(20);
         testDriver.rating.setTotalRates(5);
+        /*
         newMap.addDriver(testDriver2);
         newMap.addDriver(testDriver3);
         newMap.addDriver(testDriver4);
         newMap.addDriver(testDriver);
         newMap.addPassenger(testPassenger);
+        */
        // newMap.searchList(new Coordinates(testDriver.coordinates.getxCoord(), testDriver.coordinates.getyCoord()));
         Coordinates test = new Coordinates(100, 25);
         Coordinates test2 = new Coordinates(100, 50);
-        test.calculateDistance(test2);
-        newMap.shortestDriver(testPassenger);
+        //test.calculateDistance(test2);
+        //newMap.requestRide(newMap.passengerList.get(0), new Coordinates(299, 299));
+        testPassenger.requestRide(new Coordinates(299, 299));
+        testPassenger2.requestRide(new Coordinates(5, 5));
 
     }
 }
